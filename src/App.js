ea451759dcab2dc9d0403c66af94c4d1ccff29e3
Zoef <shaikhzoef36@gmail.com>
2022-04-28 15:52:38 +0530

    initial master branch commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Mockman from 'mockman-js';
+
+function App() {
+	return (
+		<div className='App'>
+			{/* <Mockman /> */}
+			<h1>Initial commit for master branch</h1>
+		</div>
+	);
+}
+
+export default App;
---
+++ src/backend/db/users.js
@@ -0,0 +1,18 @@
+import { v4 as uuid } from 'uuid';
+import { formatDate } from '../utils/authUtils';
+/**
+ * User Database can be added here.
+ * You can add default users of your wish with different attributes
+ * Every user will have cart (Quantity of all Products in Cart is set to 1 by default), wishList by default
+ * */
+
+export const users = [
+	{
+		_id: 1007,
+		firstName: 'Zoef',
+		email: 'Zoef@gmail.com',
+		password: '123456',
+		createdAt: formatDate(),
+		updatedAt: formatDate(),
+	},
+];
